b3d33b5d0cc23d8953a3e9fc013174d6b2392301
Claudia Font <claudiafontmarques@gmail.com>
2021-11-18 19:46:36 +0100

    passed twittersRoutes get test

--- server/routes/twittersRoutes.test.js
+++ server/routes/twittersRoutes.test.js
@@ -1,4 +1,4 @@
-/* require("dotenv").config();
+require("dotenv").config();
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const bcrypt = require("bcrypt");
@@ -8,7 +8,6 @@ const Twit = require("../../database/models/twitter");
 
 const request = supertest(app);
 let server;
-let token;
 
 beforeAll(async () => {
   await initializeMongoDB(process.env.DB_STRING_TEST);
@@ -18,10 +17,14 @@ beforeAll(async () => {
 beforeEach(async () => {
   await Twit.deleteMany();
   await Twit.create({
+    date: "2021-11-18T18:30:21.764Z",
+    _id: "61969bbd605684dcf8d48adf",
     text: "Welcome",
     likes: 3,
   });
   await Twit.create({
+    date: "2021-11-18T18:30:21.764Z",
+    _id: "61969bbd605684dcf8d48ada",
     text: "hello",
     likes: 2,
   });
@@ -36,23 +39,23 @@ afterAll((done) => {
 
 describe("Given a /twits router", () => {
   describe("When a Get request to /twits/ arrives", () => {
-    test("Then it should respond with an array of twits and a 200 status", async () => {
-      const { body } = await request.get("/twits/").expect(200);
+    test.only("Then it should respond with an array of twits and a 200 status", async () => {
+      const {
+        body: [arr1, arr2],
+      } = await request.get("/twits/").expect(200);
 
-      expect(body).toHaveLength(2);
-      expect(body).toContainEqual({ text: "Welcome", likes: 3 });
-      expect(body).toContainEqual({ text: "hello", likes: 2 });
+      expect(arr1).toEqual({
+        date: "2021-11-18T18:30:21.764Z",
+        id: "61969bbd605684dcf8d48adf",
+        text: "Welcome",
+        likes: 3,
+      });
+      expect(arr2).toEqual({
+        date: "2021-11-18T18:30:21.764Z",
+        id: "61969bbd605684dcf8d48ada",
+        text: "hello",
+        likes: 2,
+      });
     });
   });
-  describe("When a POST request to /twits/ with a twit", () => {
-    test("then it should respond with the new twit and a status 201", async () => {
-      const { body } = await request
-        .post("/twits/")
-        .send({ text: "bona nit", likes: 10 })
-        .expect(201);
-
-      expect(body).toHaveProperty("text" && "likes");
-      expect(body).toHaveProperty("likes", 10);
-    });
-  });
-}); */
+});
